22a085d044e10168b1769b55ce8a941eebca109f
ial-amanna <l.annamalai98@gmail.com>
2025-07-21 06:54:57 +0200

    Added nojekyll and other stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/my-portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  assetsInclude: ['**/*.js']
 })
